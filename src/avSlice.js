22357315312d654ca838ac2969f118d8327d2987
BlueRana <bluefrog79sl@gmail.com>
2025-04-04 18:07:32 -0400

    changed images in av

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,32 +4,32 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
     {
-        img: "https://pixabay.com/images/download/business-20031_640.jpg",
+        img: "https://cdn.pixabay.com/photo/2014/04/05/11/27/objects-315646_960_720.jpg",
         name: "Projectors",
         cost: 200,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/images/download/speakers-4109274_640.jpg",
+        img: "https://cdn.pixabay.com/photo/2016/11/14/04/42/techland-1822630_1280.jpg",
         name: "Speaker",
         cost: 35,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/images/download/public-speaking-3926344_640.jpg",
+        img: "https://cdn.pixabay.com/photo/2018/11/18/15/25/microphone-3823406_1280.jpg",
         name: "Microphones",
         cost: 45,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/images/download/whiteboard-2903269_640.png",
+        img: "https://cdn.pixabay.com/photo/2015/01/08/18/30/white-board-593370_1280.jpg",
         name: "Whiteboards",
         cost: 80,
         quantity: 0,
     },
 
     {
-        img: "https://pixabay.com/images/download/signpost-235079_640.jpg",
+        img: "https://cdn.pixabay.com/photo/2021/11/01/18/27/billboard-6761325_1280.jpg",
         name: "Signage",
         cost: 80,
         quantity: 0,
